addff561b69bba624bdcdd0aff8c5280a89138c2
vtran <trand@hochschule-trier.de>
2023-08-22 21:53:37 +0200

    Began Angular tutorial project

---
+++ Angular/angular-tutorial/src/app/app.component.ts
@@ -0,0 +1,66 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface Person {
+  name: string;
+  age: number;
+  height: number;
+  isExisting: boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  headers: string[];
+
+  title: string;
+  randomNumber: number;
+
+  imgTitle: string;
+  imgSrc: string;
+
+  inputExample: string[];
+
+  pokemonName: string;
+
+  persons: Person[];
+
+  constructor() {
+    this.headers = [
+      'String Interpolation',
+      'Property binding',
+      'Two way binding',
+      'Template reference',
+      'ngClass',
+    ];
+
+    //string interpolation
+    this.title = 'Hello World';
+    this.randomNumber = Math.random() * 10;
+
+    //property binding
+    this.imgTitle = 'Beaver Img';
+    this.imgSrc =
+      'https://media.istockphoto.com/id/180709420/de/foto/arbeit-hart.jpg?s=2048x2048&w=is&k=20&c=xajJDboUZMJh-VWWNemZjPrEvHbX1qc-gI8GCi1BoiY=';
+
+    //two way binding
+    this.inputExample = [];
+
+    //template reference
+    this.pokemonName = '';
+
+    //ngClass
+    this.persons = [
+      { name: 'anna', age: 15, height: 20, isExisting: false },
+      { name: 'hans', age: 20, height: 80, isExisting: true },
+      { name: 'laura', age: 5, height: 1, isExisting: true },
+    ];
+  }
+
+  handleClick(value: any) {
+    console.log(value.value);
+  }
+}
